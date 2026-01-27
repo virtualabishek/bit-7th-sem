23bca460769ecbff8b3b155a23befc0480548a90
virtualabishek <717abishekneupane@gmail.com>
2026-01-27 23:41:41 +0545

    done: todays assignment

--- advance-java/lab/Calculator.java
+++ advance-java/lab/Calculator.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public Calculator() {
+class Calculator {
     int a,b;
     Calculator(int a, int b) {
         this.a = a;
@@ -18,14 +18,46 @@ public Calculator() {
     int mul() {
       return a * b;
     }
-    int div() {
+    float div() {
       if(b!=0) {
-      return a/b; }
+      return a/b; 
+      } 
+      else {
+        System.out.println("Division by 0 is not possible. Try, Another Number.");
+        return 0;
+      }
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Simple Calculator");
+        System.out.println("Enter First number: ");
+        int a = sc.nextInt();
+        System.out.println("Enter second number: ");
+        int b = sc.nextInt();
+        System.out.println("Simple calculator");
         System.out.println("Instructions: ");
-        System.out.println("1. Add \n 2. Sub \n 3. Mul \n 4.")
+        System.out.println("1. Add");
+        System.out.println("2. Subtract");
+        System.out.println("3. Multiply");
+        System.out.println("4. Divide");
+        System.out.println("Enter your choise..");
+        int choice = sc.nextInt();
+        Calculator cal = new Calculator(a,b);
+        switch(choice) {
+          case 1:
+            System.out.println("Result: " + cal.add());
+            break;
+          case 2:
+            System.out.println("Result: " + cal.sub());
+            break;
+          case 3:
+            System.out.println("Result: " + cal.mul());
+            break;
+          case 4:
+            System.out.println("Result: " + cal.div());
+            break;
+          default:
+            System.out.println("Invalide choise. please see the instruction again");
+        }
+
     }
 }
