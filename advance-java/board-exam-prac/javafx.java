6ba18f2b6605b5445173212b22c2dca482abf327
Abishek Neupane <717abishekneupane@gmail.com>
2026-06-18 06:25:54 +0545

    Create javafx.java

---
+++ advance-java/board-exam-prac/javafx.java
@@ -0,0 +1,57 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class LoginForm extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        // Labels
+        Label userLabel = new Label("Username:");
+        Label passLabel = new Label("Password:");
+
+        // Input fields
+        TextField userField = new TextField();
+        PasswordField passField = new PasswordField();
+
+        // Button
+        Button loginButton = new Button("Login");
+
+        // Layout
+        GridPane grid = new GridPane();
+        grid.setVgap(10);
+        grid.setHgap(10);
+
+        grid.add(userLabel, 0, 0);
+        grid.add(userField, 1, 0);
+
+        grid.add(passLabel, 0, 1);
+        grid.add(passField, 1, 1);
+
+        grid.add(loginButton, 1, 2);
+
+        // Button action
+        loginButton.setOnAction(e -> {
+            String username = userField.getText();
+            String password = passField.getText();
+
+            if (username.equals("admin") && password.equals("1234")) {
+                System.out.println("Login Successful");
+            } else {
+                System.out.println("Invalid Credentials");
+            }
+        });
+
+        Scene scene = new Scene(grid, 300, 150);
+        primaryStage.setTitle("Login Form");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
